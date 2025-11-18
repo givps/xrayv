64760846bba1199685adff499e15dcd732a1bd5c
gilper0x <gilper0x@gmail.com>
2025-11-18 18:30:55 +0700

    Create cf-proxy.sh

---
+++ setting/cf-proxy.sh
@@ -0,0 +1,23 @@
+iptables -D INPUT -p tcp --dport 80 -j ACCEPT 2>/dev/null
+iptables -D INPUT -p tcp --dport 443 -j ACCEPT 2>/dev/null
+
+for i in {1..5}; do
+    iptables -D INPUT -p tcp --dport 80 -j ACCEPT 2>/dev/null
+    iptables -D INPUT -p tcp --dport 443 -j ACCEPT 2>/dev/null
+done
+
+for ip in $(curl -s https://www.cloudflare.com/ips-v4); do
+    iptables -A INPUT -p tcp -s $ip --dport 80 -j ACCEPT
+    iptables -A INPUT -p tcp -s $ip --dport 443 -j ACCEPT
+done
+
+iptables -A INPUT -p tcp --dport 80 -j DROP
+iptables -A INPUT -p tcp --dport 443 -j DROP
+
+iptables -D INPUT 6
+
+apt install iptables-persistent -y
+netfilter-persistent save
+netfilter-persistent reload
+
+reboot
